f11c9c8c2346aba39838a7a2e73dea02b18910f3
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2021-09-21 07:44:49 -0400

    Quick commit before going to work. Got some stuff done, mostly just getting a better grasp on the Google Map API

--- Final_Project/app/src/main/java/com/example/forager/MapsActivity.kt
+++ Final_Project/app/src/main/java/com/example/forager/MapsActivity.kt
@@ -3,8 +3,10 @@ package com.example.forager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -28,6 +30,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val typeAndStyles by lazy { TypeAndStyles() }
     private val  cameraAndViewport by lazy { CameraAndViewport() }
+
+    private val grav by lazy { Gravity() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,28 +72,82 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val marquette = LatLng(46.5436, -87.3954)
         val newYork = LatLng(40.71614203933524, -74.0040676650565)
         map.addMarker(MarkerOptions().position(marquette).title("Marquette Marker"))
-        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(marquette, 10f))
-        map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraAndViewport.marquette))
-
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(marquette, 10f))
+        //map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraAndViewport.marquette))
+        typeAndStyles.setMapStyle(map, this) // Sets the style of my map using raw JSON
         map.uiSettings.apply {
             isZoomControlsEnabled = true
-            // I think I want this enabled, need to come back to this
-            // I'll first need to enable my-location layer
-            // isMyLocationButtonEnabled = true
-        }
-        typeAndStyles.setMapStyle(map, this)
-
-        // This is from the last video I watched in the Google API tutorial
-        // Don't keep this!
-        lifecycleScope.launch {
-            delay(4000L)
-            map.moveCamera(CameraUpdateFactory.newLatLng(newYork))
         }
 
-        // I think I want some padding on the bottom for my buttons and other options
-        // Keeping this commented so I can come back to it
-        // map.setPadding(0, 0, 0, 300)
+        onMapClicked()
+        onMapLongClicked()
     }
 
+    private fun onMapClicked() {
+        map.setOnMapClickListener {
+            val latLong = it.latitude.toString() + ", " + it.longitude.toString()
+            Toast.makeText(this, "Short click on: $latLong", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun onMapLongClicked() {
+        map.setOnMapLongClickListener {
+            map.addMarker(MarkerOptions().position(it).title("Your new marker!"))
+            grav.apply {
+                Toast.makeText(this@MapsActivity, "New marker at: ${it.longitude}, ${it.longitude}", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
 }
+
+/*
+map.uiSettings.apply {
+    isZoomControlsEnabled = true
+    // I think I want this enabled, need to come back to this
+    // I'll first need to enable my-location layer
+    // isMyLocationButtonEnabled = true
+}
+typeAndStyles.setMapStyle(map, this)
+
+lifecycleScope.launch {
+    delay(4000L)
+
+    // animates the zooming in OR out depending on the zoom value given (between 0..20)
+    //map.animateCamera(CameraUpdateFactory.zoomTo(15f), 2000, null)
+
+    // This will animate the moving of the camera to a specified location in pixels, with a given duration and a GoogleMap.CancelableCallback
+    //map.animateCamera(CameraUpdateFactory.scrollBy(200f, 0f), 2000, null)
+
+    // This animates all of the properties from cameraAndViewport.marquette in CameraAndViewport.kt
+    // In this final version I use a GoogleMaps.CancelableCallback as the callback, I override two functions "onFinish()" and "onCancel()" and display a Toast message
+    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraAndViewport.melbourne), 2000, object: GoogleMap.CancelableCallback {
+
+        // Toast text will display when the camera animation is finished
+        override fun onFinish() {
+            Toast.makeText(this@MapsActivity, "Finished", Toast.LENGTH_SHORT).show()
+        }
+
+        // Toast text will display when/if the camera animation is canceled
+        override fun onCancel() {
+            Toast.makeText(this@MapsActivity, "Canceled", Toast.LENGTH_SHORT).show()
+        }
+    })
+
+    //map.animateCamera(CameraUpdateFactory.newLatLngBounds(cameraAndViewport.melbourneBounds, 100), 2000, null)
+    // map.setLatLngBoundsForCameraTarget(cameraAndViewport.melbourneBounds)
+*/
+
+
+// This would all go in the "map.uiSettings.apply {  } block
+
+// This is from the last video I watched in the Google API tutorial
+// Don't keep this!
+// lifecycleScope.launch {
+//     delay(4000L)
+//     map.moveCamera(CameraUpdateFactory.scrollBy(-1000f, 300f))
+// }
+
+// I think I want some padding on the bottom for my buttons and other options
+// Keeping this commented so I can come back to it
+// map.setPadding(0, 0, 0, 300)
--- Final_Project/app/src/main/java/com/example/forager/misc/TypeAndStyles.kt
+++ Final_Project/app/src/main/java/com/example/forager/misc/TypeAndStyles.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.util.Log
 import android.view.MenuItem
 import com.example.forager.R
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.MapStyleOptions
 import java.lang.Exception
 
 class TypeAndStyles {
+
+    private val cameraAndViewport by lazy { CameraAndViewport() }
 
     fun setMapStyle(googleMap: GoogleMap, context: Context) {
         try {
@@ -25,23 +28,32 @@ class TypeAndStyles {
         }
     }
 
+    // This originally was doing the work up changing the map TYPES
+    // Changed it to move to different locations using the "animateCamera()" function
+    // Will want to use this when moving from pin to pin when the user pins a location!
     fun setMapType(item: MenuItem, googleMap: GoogleMap) {
         when(item.itemId) {
-            R.id.normal_map -> {
-                googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.jump_to_melbourne -> {
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(cameraAndViewport.melbourneBounds, 100), 8000, null)
             }
-            R.id.hybrid_map -> {
-                googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+            R.id.jump_to_marquette -> {
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(cameraAndViewport.marquetteBounds, 100), 8000, null)
             }
-            R.id.satellite_map -> {
-                googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
-            }
-            R.id.terrain_map -> {
-                googleMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
-            }
-            R.id.none_map -> {
-                googleMap.mapType = GoogleMap.MAP_TYPE_NONE
-            }
+//            R.id.normal_map -> {
+//                googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+//            }
+//            R.id.hybrid_map -> {
+//                googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+//            }
+//            R.id.satellite_map -> {
+//                googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+//            }
+//            R.id.terrain_map -> {
+//                googleMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+//            }
+//            R.id.none_map -> {
+//                googleMap.mapType = GoogleMap.MAP_TYPE_NONE
+//            }
         }
     }
 
